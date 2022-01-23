7318e6c05ff618a1a1a622baa67c192cc6c0b467
farhah din <farhahdin97@hotmail.com>
2022-01-23 17:18:02 +0000

    acesing elements

---
+++ script.js
@@ -0,0 +1,19 @@
+// Easy access to elements
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+// Get access to the OpenWeather API
+var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
+
+// Easy access to data
+var cityList = [];
