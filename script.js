2fa5b2491b28c61478e5eca1a40807ac5776577a
farhah din <farhahdin97@hotmail.com>
2022-01-23 18:23:56 +0000

    displaying and saving the search histroy but need to make a function to list the arrays in order for this function to work

--- script.js
+++ script.js
@@ -164,11 +164,58 @@ function currentConditionsRequest(searchValue) {
         });
 
     });
-
-    
-
 };
 
 
+// Display and save the search history of cities
+function searchHistory(searchValue) {
+    // Grab value entered into search bar 
+    // var searchValue = searchCityInput.val().trim();
+    
+    // If there are characters entered into the search bar
+    if (searchValue) {
+        // Place value in the array of cities
+        // if it is a new entry
+        if (cityList.indexOf(searchValue) === -1) {
+            cityList.push(searchValue);
 
+            // List all of the cities in user history
+            listArray();
+            clearHistoryButton.removeClass("hide");
+            weatherContent.removeClass("hide");
+        } else {
+            // Remove the existing value from
+            // the array
+            var removeIndex = cityList.indexOf(searchValue);
+            cityList.splice(removeIndex, 1);
 
+            // Push the value again to the array
+            cityList.push(searchValue);
+
+            // list all of the cities in user history
+            // so the old entry appears at the top
+            // of the search history
+            listArray();
+            clearHistoryButton.removeClass("hide");
+            weatherContent.removeClass("hide");
+        }
+    }
+    // console.log(cityList);
+}
+
+// List the array into the search history sidebar
+function listArray() {
+    // Empty out the elements in the sidebar
+    searchHistoryList.empty();
+    // Repopulate the sidebar with each city
+    // in the array
+    cityList.forEach(function(city){
+        var searchHistoryItem = $('<li class="list-group-item city-btn">');
+        searchHistoryItem.attr("data-value", city);
+        searchHistoryItem.text(city);
+        searchHistoryList.prepend(searchHistoryItem);
+    });
+    // Update city list history in local storage
+    localStorage.setItem("cities", JSON.stringify(cityList));
+    
+}
